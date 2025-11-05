8d99b428979d274bf72d4082ec2d8472c3e22ae2
Andriy Polukhin <andriy.polukhin@gmail.com>
2025-11-05 16:13:19 +0200

    initial commit

--- packages/functions/src/api.ts
+++ packages/functions/src/api.ts
@@ -1,10 +1,10 @@
-import { Resource } from "sst";
-import { Handler } from "aws-lambda";
-import { Example } from "@monorepo-template/core/example";
+import { Resource } from 'sst'
+import { Handler } from 'aws-lambda'
+import { Example } from '@sst-notes/core/example'
 
 export const handler: Handler = async (_event) => {
-  return {
-    statusCode: 200,
-    body: `${Example.hello()} Linked to ${Resource.MyBucket.name}.`,
-  };
-};
+	return {
+		statusCode: 200,
+		body: `${Example.hello()} Linked to ${Resource.Uploads.name}.`,
+	}
+}
