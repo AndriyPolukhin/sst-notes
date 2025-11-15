05dd91e3c2310673e1f1974d6a22a08852731c01
Andriy Polukhin <andriy.polukhin@gmail.com>
2025-11-16 01:02:40 +0200

    fix to the route

--- packages/frontend/src/components/UnauthenticatedRoute.tsx
+++ packages/frontend/src/components/UnauthenticatedRoute.tsx
@@ -2,7 +2,7 @@ import { cloneElement, type ReactElement } from 'react'
 import { Navigate } from 'react-router-dom'
 import { useAppContext } from '../lib/contextLib'
 
-function querystring(name: string, url, url = window.location.href) {
+function querystring(name: string, url = window.location.href) {
 	const parsedName = name.replace(/[[]]/g, '\\$&')
 	const regex = new RegExp(`[?&]${parsedName}(=([^&#]*)|&|#|$)`, 'i')
 	const results = regex.exec(url)
@@ -18,12 +18,13 @@ interface Props {
 	children: ReactElement
 }
 
-export default function UnauthenticatedRoute(props: Props): ReactElement {
+export default function UnauthenticatedRoute(props: Props) {
 	const { isAuthenticated } = useAppContext()
 	const { children } = props
+	const redirect = querystring('redirect')
 
 	if (isAuthenticated) {
-		return <Navigate to={'/'} />
+		return <Navigate to={redirect || '/'} />
 	}
 
 	return cloneElement(children, props)
